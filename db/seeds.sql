bbd60739a2e898947ab40f16b59080fb52157035
mehdicode <kalan.mahdi@yahoo.com>
2017-06-24 23:44:09 -0700

    finished

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('mushroom burger');
+INSERT INTO burgers (burger_name) VALUES ('megamac burger');
